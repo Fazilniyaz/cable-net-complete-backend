af355da7ba860c824eef53ec31a364ac8ce7f3bf
Fazil Niyazdeen TM <113542759+Fazilniyaz@users.noreply.github.com>
2025-11-01 22:15:55 +0530

    .

--- server/server.js
+++ server/server.js
@@ -58,6 +58,7 @@ const adminSchema = new mongoose.Schema({
 });
 
 const Admin = mongoose.models.Admin || mongoose.model("Admin", adminSchema);
+module.exports = Admin;
 
 // JWT Middleware
 const authenticateToken = (req, res, next) => {
